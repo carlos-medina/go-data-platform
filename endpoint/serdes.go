d80fe53558451527985a6809a2afad148bd16b29
Carlos <carlosenmm@gmail.com>
2023-11-01 18:18:25 -0300

    Implementend input unmarshal and its validation
    - Added package endpoint with Input struct and functions for the data unmarshal and its validation so no field can have a zero value

---
+++ endpoint/serdes.go
@@ -0,0 +1,62 @@
+package endpoint
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/arquivei/foundationkit/errors"
+)
+
+type Input struct {
+	UserID  int    `json:"user_id"`
+	DataID  int    `json:"data_id"`
+	Version int    `json:"version"`
+	Content string `json:"content"`
+}
+
+// DecodeInput validates if all Input fields have non zero values
+// after doing the data unmarshal
+func DecodeInput(inputBytes []byte) (Input, error) {
+	const op = errors.Op("endpoint.DecodeInput")
+
+	input, err := UnmarshalInput(inputBytes)
+
+	if err != nil {
+		return Input{}, errors.E(op, err)
+	}
+
+	var zeroValueFileds []string
+
+	if input.UserID == 0 {
+		zeroValueFileds = append(zeroValueFileds, "UserID")
+	}
+	if input.DataID == 0 {
+		zeroValueFileds = append(zeroValueFileds, "DataID")
+	}
+	if input.Version == 0 {
+		zeroValueFileds = append(zeroValueFileds, "Version")
+	}
+	if input.Content == "" {
+		zeroValueFileds = append(zeroValueFileds, "Content")
+	}
+
+	if zeroValueFileds != nil {
+		return Input{}, errors.E(op, errors.New(fmt.Sprintf("Zero value fields: %v", zeroValueFileds)))
+	}
+
+	return input, nil
+}
+
+// UnmarshalInput unmarshal the input in its struct
+func UnmarshalInput(inputBytes []byte) (Input, error) {
+	const op = errors.Op("endpoint.UnmarshalInputData")
+
+	var inputData Input
+
+	err := json.Unmarshal(inputBytes, &inputData)
+	if err != nil {
+		return Input{}, errors.E(op, err)
+	}
+
+	return inputData, nil
+}
---
+++ endpoint/serted_test.go
@@ -0,0 +1,132 @@
+package endpoint
+
+import (
+	"testing"
+
+	"github.com/arquivei/foundationkit/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecodeInput(t *testing.T) {
+	tests := []struct {
+		name          string
+		inputBytes    []byte
+		expecteResult Input
+		expectedError string
+	}{
+		{
+			name:          "Failure: Simple string",
+			inputBytes:    []byte("aldsls"),
+			expectedError: "invalid character 'a' looking for beginning of value",
+		},
+		{
+			name:          "Failure - Missing UserID",
+			inputBytes:    []byte("{\"data_id\":1,\"version\":1,\"content\":\"soap\"}"),
+			expectedError: "Zero value fields: [UserID]",
+		},
+		{
+			name:          "Failure - Missing DataID",
+			inputBytes:    []byte("{\"user_id\":1,\"version\":1,\"content\":\"soap\"}"),
+			expectedError: "Zero value fields: [DataID]",
+		},
+		{
+			name:          "Failure - Missing Version",
+			inputBytes:    []byte("{\"user_id\":1,\"data_id\":1,\"content\":\"soap\"}"),
+			expectedError: "Zero value fields: [Version]",
+		},
+		{
+			name:          "Failure - Missing Content",
+			inputBytes:    []byte("{\"user_id\":1,\"data_id\":1,\"version\":1}"),
+			expectedError: "Zero value fields: [Content]",
+		},
+		{
+			name:          "Failure - Missing DataID and Content",
+			inputBytes:    []byte("{\"user_id\":1,\"version\":1}"),
+			expectedError: "Zero value fields: [DataID Content]",
+		},
+		{
+			name:          "Failure - Missing all fields",
+			inputBytes:    []byte("{}"),
+			expectedError: "Zero value fields: [UserID DataID Version Content]",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			inputData, err := DecodeInput(test.inputBytes)
+			if test.expectedError == "" {
+				assert.NoError(t, err)
+				assert.Equal(t, test.expecteResult, inputData)
+			} else {
+				assert.EqualError(t, errors.GetRootErrorWithKV(err), test.expectedError)
+			}
+		})
+	}
+}
+
+func TestUnmarshalInputData(t *testing.T) {
+	tests := []struct {
+		name          string
+		inputBytes    []byte
+		expecteResult Input
+		expectedError string
+	}{
+		{
+			name:          "Failure: Simple string",
+			inputBytes:    []byte("aldsls"),
+			expectedError: "invalid character 'a' looking for beginning of value",
+		},
+		{
+			name:          "Failure: Empty string",
+			inputBytes:    []byte(""),
+			expectedError: "unexpected end of JSON input",
+		},
+		{
+			name:          "Failure: Incorrect type int when expected string",
+			inputBytes:    []byte("{\"user_id\":1,\"data_id\":1,\"version\":1,\"content\":1}"),
+			expectedError: "json: cannot unmarshal number into Go struct field Input.content of type string",
+		},
+		{
+			name:       "Success - Empty map",
+			inputBytes: []byte("{}"),
+			expecteResult: Input{
+				UserID:  0,
+				DataID:  0,
+				Version: 0,
+				Content: "",
+			},
+		},
+		{
+			name:       "Success - Missing value from key",
+			inputBytes: []byte("{\"user_id\":1,\"data_id\":1,\"version\":1}"),
+			expecteResult: Input{
+				UserID:  1,
+				DataID:  1,
+				Version: 1,
+				Content: "",
+			},
+		},
+		{
+			name:       "Success - All keys have values",
+			inputBytes: []byte("{\"user_id\":1,\"data_id\":1,\"version\":1,\"content\":\"soap\"}"),
+			expecteResult: Input{
+				UserID:  1,
+				DataID:  1,
+				Version: 1,
+				Content: "soap",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			inputData, err := UnmarshalInput(test.inputBytes)
+			if test.expectedError == "" {
+				assert.NoError(t, err)
+				assert.Equal(t, test.expecteResult, inputData)
+			} else {
+				assert.EqualError(t, errors.GetRootErrorWithKV(err), test.expectedError)
+			}
+		})
+	}
+}
